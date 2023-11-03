bf959c739c018d68f77b2cf72cbe0a6036c3710a
Vaidik Sharma <chrisharry1281@gmail.com>
2023-11-03 22:50:04 +0530

    adding maps using api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import './App.css';
+import { useRef, useEffect, useState } from 'react'
+import * as tt from '@tomtom-international/web-sdk-maps'
+import './App.css'
+import '@tomtom-international/web-sdk-maps/dist/maps.css'
 
 function App() {
+  const mapElement = useRef()
+  const [map, setMap] = useState({})
+
+  useEffect(() => {
+    const origin = {
+      lng: 77.2090, // New Delhi's longitude
+      lat: 28.6139, // New Delhi's latitude
+    }
+    const destinations = []
+
+    let map = tt.map({
+      key: process.env.REACT_APP_TOM_TOM_API_KEY,
+      container: mapElement.current,
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true,
+      },
+      center: [origin.lng, origin.lat],
+      zoom: 14,
+    })
+    setMap(map)
+  }, []) // The empty dependency array means this effect will run once, similar to componentDidMount.
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {map && (
+        <div className="app">
+          <div ref={mapElement} className="map" />
+          <div className="search-bar">
+            <h1>Where to?</h1>
+            <input
+              type="text"
+              id="longitude"
+              className="longitude"
+              placeholder="Longitude"
+            />
+            <input
+              type="text"
+              id="latitude"
+              className="latitude"
+              placeholder="Latitude"
+            />
+          </div>
+        </div>
+      )}
+    </>
+  )
 }
 
 export default App;
